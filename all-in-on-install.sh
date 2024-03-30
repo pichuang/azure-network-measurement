77f202302eaa828ee5cba5239b3a9d348631033b
Phil Huang <phil.huang@microsoft.com>
2024-03-31 03:07:00 +0800

    x

--- all-in-on-install.sh
+++ all-in-on-install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ./2ping/install-2ping-ubuntu.sh
+./2ping/server/install-systemd-service.sh
 ./asn/install-asn-ubuntu.sh
+./asn/server/install-systemd-service.sh
 ./iperf3/install-iperf3-ubuntu.sh
+./iperf3/server/install-systemd-service.sh
 ./sockperf/install-sockperf-ubuntu.sh
+./sockperf/server/install-systemd-service.sh
