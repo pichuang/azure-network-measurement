f4ba2e4791639b226f545e839956d2dd2f5b8525
Phil Huang <phil.huang@microsoft.com>
2024-03-31 08:21:12 +0800

    xx

--- sockperf/client/sockperf-client-command.sh
+++ sockperf/client/sockperf-client-command.sh
@@ -2,9 +2,9 @@
 echo "=============="
 echo "Public Routing"
 echo "=============="
-sockperf ping-pong -i 70.157.24.53 --tcp --time 30 --msg-size 1500 --full-rtt
+sockperf ping-pong -i 70.157.24.53 --tcp --time 30 --msg-size 1500 --mps=max --full-rtt
 
 echo "==============="
 echo "Private Routing"
 echo "==============="
-sockperf ping-pong -i 10.100.2.38 --tcp --time 30 --msg-size 1500 --full-rtt
+sockperf ping-pong -i 10.100.2.38 --tcp --time 30 --msg-size 1500 --mps=max --full-rtt
