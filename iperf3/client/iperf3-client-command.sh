aad0674d5a0ebf0605108de59645d4eaad2dc6a9
Phil Huang <phil.huang@microsoft.com>
2024-05-14 00:34:56 +0800

    Update iperf3-client-command.sh

--- iperf3/client/iperf3-client-command.sh
+++ iperf3/client/iperf3-client-command.sh
@@ -3,15 +3,15 @@
 echo "=============="
 echo "Public Routing"
 echo "=============="
-iperf3 --client 70.157.24.53  --time 30 --interval 1 --omit 1 --bidir
+iperf3 --client 70.157.24.53  --time 30 --interval 1 --omit 1 --bidir --parallel 32
 # or
-# iperf3 --client 70.157.24.53  --time 30 --interval 1 --omit 1
-# iperf3 --client 70.157.24.53  --time 30 --interval 1 --reverse --omit 1
+# iperf3 --client 70.157.24.53  --time 30 --interval 1 --omit 1 --parallel 32
+# iperf3 --client 70.157.24.53  --time 30 --interval 1 --reverse --omit 1 --parallel 32
 
 echo "=============="
 echo "Private Routing"
 echo "=============="
-iperf3 --client 10.100.2.38 --time 30 --interval 1 --omit 1 --bidir
+iperf3 --client 10.100.2.38 --time 30 --interval 1 --omit 1 --bidir --parallel 32
 # or
-# iperf3 --client 10.100.2.38 --time 30 --interval 1 --omit 1
-# iperf3 --client 10.100.2.38 --time 30 --interval 1 --reverse --omit 1
+# iperf3 --client 10.100.2.38 --time 30 --interval 1 --omit 1 --parallel 32
+# iperf3 --client 10.100.2.38 --time 30 --interval 1 --reverse --omit 1 --parallel 32
