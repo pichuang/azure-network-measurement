f00e42ee129b677f9714c63e51f9737c921daa8b
Phil Huang <phil.huang@microsoft.com>
2024-10-02 15:28:09 +0800

    chore: Update all-in-one-install.sh script

--- all-in-one-install.sh
+++ all-in-one-install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-./deadman/install-deadman.sh
-./2ping/install-2ping-ubuntu.sh
-./2ping/server/install-systemd-service.sh
-./asn/install-asn-ubuntu.sh
-./asn/server/install-systemd-service.sh
-./iperf3/install-iperf3-ubuntu.sh
-./iperf3/server/install-systemd-service.sh
-./sockperf/install-sockperf-ubuntu.sh
-./sockperf/server/install-systemd-service.sh
+# Install deadman
+./deadman/install-deadman.sh || true
+# Install 2ping
+./2ping/install-2ping-ubuntu.sh || true
+./2ping/server/install-systemd-service.sh || true
+# Install asn
+./asn/install-asn-ubuntu.sh || true
+./asn/server/install-systemd-service.sh || true
+# Install iperf3
+./iperf3/install-iperf3-ubuntu.sh || true
+./iperf3/server/install-systemd-service.sh || true
+# Install sockperf
+./sockperf/install-sockperf-ubuntu.sh || true
+./sockperf/server/install-systemd-service.sh || true
